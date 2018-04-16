2ecaa3aa954248f287b7b3c871b1f8ee963ad801
Raphael Mouton <raphaelmouton@live.fr>
2018-04-16 19:49:12 +0200

    ajout crud

---
+++ src/AppBundle/Entity/reservation.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * reservation
+ *
+ * @ORM\Table(name="reservation")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\reservationRepository")
+ */
+class reservation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nbReservedSeats", type="smallint")
+     */
+    private $nbReservedSeats;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publicationDate", type="datetime")
+     */
+    private $publicationDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="wasDone", type="boolean")
+     */
+    private $wasDone;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nbReservedSeats
+     *
+     * @param integer $nbReservedSeats
+     *
+     * @return reservation
+     */
+    public function setNbReservedSeats($nbReservedSeats)
+    {
+        $this->nbReservedSeats = $nbReservedSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbReservedSeats
+     *
+     * @return int
+     */
+    public function getNbReservedSeats()
+    {
+        return $this->nbReservedSeats;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param \DateTime $publicationDate
+     *
+     * @return reservation
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return \DateTime
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Set wasDone
+     *
+     * @param boolean $wasDone
+     *
+     * @return reservation
+     */
+    public function setWasDone($wasDone)
+    {
+        $this->wasDone = $wasDone;
+
+        return $this;
+    }
+
+    /**
+     * Get wasDone
+     *
+     * @return bool
+     */
+    public function getWasDone()
+    {
+        return $this->wasDone;
+    }
+}
+
